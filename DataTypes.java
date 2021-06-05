a3724b2c2d39f1e40185704a3b752556c9ca9265
WahyudinAkbar <wahyu.id.dev@gmail.com>
2021-06-05 16:09:57 +0800

    Deklarasi Variabel dan Data

---
+++ DataTypes.java
@@ -0,0 +1,12 @@
+public class DataTypes {
+  public static void main(String [] args) {
+    String namaDepan = "M";
+    String namaTengah = "Wahyudin";
+    String namaBelakang = "Akbar";
+    int usia = 20;
+    int targetTahunKuliah = 4;
+    double ipk = 3.777;
+    char nilaiAbjad = 'A';
+    boolean tampan = true;
+  }
+}
